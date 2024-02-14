de032448fe9dd771a86941cc834860d07847517e
keanderss <keanders@abo.fi>
2024-02-14 21:09:34 +0200

    test stuff

--- src/test/java/io/github/keanderss/codetest/CodetestApplicationTests.java
+++ src/test/java/io/github/keanderss/codetest/CodetestApplicationTests.java
@@ -1,18 +1,29 @@
 package io.github.keanderss.codetest;
 
+import io.github.keanderss.codetest.model.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CodetestApplicationTests {
 
+    @Test
+    void testCustomer() {
+        String testCustomerName = "NAME";
+        double testCustomerLoan = 100;
+        double testCustomerInterest = 1.25;
+        int testCustomerYears = 10;
+        Customer testCustomer = new Customer(testCustomerName, testCustomerLoan, testCustomerInterest, testCustomerYears);
+        System.out.println(testCustomer.getName());
+        System.out.println(testCustomer.getLoan());
+        System.out.println(testCustomer.getInterest());
+        System.out.println(testCustomer.getYears());
+        System.out.println(testCustomer.getMonthlyPayment());
+    }
+
 //    @Test
 //    void contextLoads() {
 //    }
 // TODO:
-// PING DATABASE
-// PING SERVER
-// TEST THAT GIVEN CUSTOMERS ARE IN DATABASE
 // TEST THAT MORTGAGE CALCULATIONS FOR CUSTOMERS ARE CORRECT
-// TEST OTHER FUNCTIONS
 }
